712f6f0f7a53d25c6330648816abc7b6bed7931d
ripal-designer <55450429+ripal-designer@users.noreply.github.com>
2022-06-01 11:21:57 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const { required } = require("nodemon/lib/config");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-
-
 
 //static files
 app.use(express.static("public"));
